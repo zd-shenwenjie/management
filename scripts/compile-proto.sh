14eaf82a5ee5050391012cc0e2a1fdc313e691dc
shenwenjie <wenjie.shen@zd-automotive.de>
2021-06-15 11:31:25 +0200

    add mkdir ./src/lib/proto

--- scripts/compile-proto.sh
+++ scripts/compile-proto.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
-
 OUT_DIR="./src/lib/proto"
 IN_DIR="./proto"
 PROTOC="$(npm bin)/grpc_tools_node_protoc"
 PROTOC_GEN_TS_PATH="$(npm bin)/protoc-gen-ts"
 PROTOC_GEN_GRPC_PATH="$(npm bin)/grpc_tools_node_protoc_plugin"
+
+mkdir -p $OUT_DIR
 
 $PROTOC \
     -I="./proto/" \
